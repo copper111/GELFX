a1dacb0a7b723bede7a52357575a4e9a196dc854
aer <pythonskill@yandex.ru>
2016-11-25 04:54:55 +0300

    Пример Javafx+ spring не потерять

---
+++ src/main/java/ru/gelfx/services/BashComandService.java
@@ -0,0 +1,13 @@
+package ru.gelfx.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by aer on 25.11.16.
+ */
+@Service
+public class BashComandService {
+    public String getHello(){
+        return "Hello";
+    }
+}
--- src/main/java/ru/gelfx/ui/MainController.java
+++ src/main/java/ru/gelfx/ui/MainController.java
@@ -1,7 +1,25 @@
 package ru.gelfx.ui;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.gelfx.services.BashComandService;
+
 /**
  * Created by aer on 25.11.16.
  */
+
+
 public class MainController {
+
+    public Button bashEnter;
+    public Label bashOut;
+
+    @Autowired
+    private BashComandService bashComandService;
+
+    public void onMouseClick(MouseEvent mouseEvent) {
+        bashOut.setText(bashComandService.getHello());
+    }
 }
